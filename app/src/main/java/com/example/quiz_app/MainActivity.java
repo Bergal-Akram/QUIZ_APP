13b5df65908c5ba2d32513a80501c2cc5fc1d55e
Ayman6965 <akramdz0011@gmail.com>
2025-04-08 18:08:25 +0100

    ..

--- app/src/main/java/com/example/quiz_app/MainActivity.java
+++ app/src/main/java/com/example/quiz_app/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.quiz_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,11 +26,24 @@ public class MainActivity extends AppCompatActivity {
 
         TextView reset = findViewById(R.id.frg_log);
         Button login = findViewById(R.id.btn_log);
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+                String text_username = username.getText().toString();
+                String text_password = password.getText().toString();
 
+                if(text_username.equals("akram@gmail.com") && text_password.equals("password0123456789")){
+                    Intent intent = new Intent(MainActivity.this , MainActivity1.class);
+                    intent.putExtra("user_name",text_username);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"username or password are wrong",Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
+
     }
 }
